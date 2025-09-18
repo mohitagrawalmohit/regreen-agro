9162eee9e9a54c67e3ca45f653196804e7c485a7
mohitagrawalmohit <mohitagarwal00112@gmail.com>
2025-09-18 17:35:07 +0530

    fix updates

--- components/CategoryProducts.jsx
+++ components/CategoryProducts.jsx
@@ -219,7 +219,7 @@ useEffect(() => {
                 onClick={() => router.push(`/product/${product.id}`)}
                 className="bg-white w-full h-auto max-w-[250px] md:max-w-[280px] mx-auto md:mx-20 rounded-[20px] shadow-md hover:shadow-lg transition duration-300 overflow-hidden cursor-pointer"
               >
-                <div className="bg-white p-3 md:p-4 flex flex-col justify-center items-start">
+                <div className="bg-white p-3 md:p-4 flex flex-col justify-center items-start h-40 md:h-44">
                   <h3 className={`font-semibold mb-1 ${mobileCols === 1 ? "text-medium md:text-xl" : "text-[10px] md:text-lg"}`}>
                     {product.cc}{" | "}{product.title}
                   </h3>
