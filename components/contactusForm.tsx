fc244469cf175fd768bef84e2a9107d691a7c5cc
mohitagrawalmohit <mohitagarwal00112@gmail.com>
2025-10-02 20:29:02 +0530

    fix everything

--- components/contactusForm.tsx
+++ components/contactusForm.tsx
@@ -133,12 +133,46 @@ export default function ContactForm() {
                 className="w-full md:max-w-[350px] md:ml-7 p-3 rounded-lg border border-white/20 bg-white/100 text-black placeholder-black/30 appearance-none pr-10 focus:outline-none focus:ring-2 focus:ring-emerald-400"
               >
                 <option className= "text-black/30 "value="">Type of Machinery Needed</option>
-            <option value="tiller">Tillers</option>
-            <option value="seeder">Seeders</option>
-            <option value="sprayer">Sprayers</option>
-            <option value="harvester">Harvesting Machines</option>
-            <option value="pump">Pumps</option>
-            <option value="storage">Storage Solutions</option>
+            <option value="mini-tiller">Mini Tiller</option>
+<option value="rotary-weeder">Rotary Weeder</option>
+<option value="ploughs-cultivators">Ploughs & Cultivators</option>
+<option value="depth-controller">Depth Controller</option>
+<option value="leveler">Leveler</option>
+<option value="ridgers">Ridgers</option>
+<option value="rotary-tiller-blades">Rotary Tiller Blades</option>
+<option value="earth-auger">Earth Auger</option>
+<option value="earth-auger-attachments">Earth Auger Attachments</option>
+<option value="water-pump">Water Pump</option>
+<option value="hose-pipe">Hose Pipe</option>
+<option value="sprayer">Sprayer</option>
+<option value="garden-sprayer">Garden Sprayer</option>
+<option value="ulv-sprayer">ULV Sprayer</option>
+<option value="mist-sprayer">Mist Sprayer</option>
+<option value="thermal-fogging-machine">Thermal Fogging Machine</option>
+<option value="power-reaper">Power Reaper</option>
+<option value="brush-cutter">Brush Cutter</option>
+<option value="power-pruner">Power Pruner</option>
+<option value="tea-pruner">Tea Pruner</option>
+<option value="tea-harvester">Tea Harvester</option>
+<option value="chainsaw">Chainsaw</option>
+<option value="wood-cutter">Wood Cutter</option>
+<option value="flour-mill">Flour Mill</option>
+<option value="rice-mill">Rice Mill</option>
+<option value="thresher">Thresher</option>
+<option value="chaff-cutter">Chaff Cutter</option>
+<option value="lawn-mower">Lawn Mower</option>
+<option value="garden-sprayer-hand-held">Garden Sprayer (Hand-held)</option>
+<option value="seeder">Seeder</option>
+<option value="planter">Planter</option>
+<option value="wood-chipper">Wood Chipper</option>
+<option value="sheep-wool-clipper">Sheep Wool Clipper</option>
+<option value="car-washer">Car Washer</option>
+<option value="snow-thrower">Snow Thrower</option>
+<option value="disc-harrow">Disc Harrow</option>
+<option value="petrol-engine">Petrol Engine</option>
+<option value="diesel-engine">Diesel Engine</option>
+
+
           
               </select>
               <ChevronDown className="absolute md:mr-10 right-3 top-1/2 -translate-y-1/2 text-black pointer-events-none" size={20} />
