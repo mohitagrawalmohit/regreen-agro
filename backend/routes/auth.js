cbad83ce43340495422dc27b00e3e4c76e203bb0
mohitagrawalmohit <mohitagarwal00112@gmail.com>
2025-10-16 03:08:14 +0530

    fix new things

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ import express from "express";
 import pool from "../db.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
