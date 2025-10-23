53b9999404842cac3b88d037f3394967b5a0e333
mohitagrawalmohit <mohitagarwal00112@gmail.com>
2025-10-23 15:07:11 +0530

    add google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,22 +24,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* ✅ Google Tag Manager (Head) */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){
-              w[l]=w[l]||[];
-              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),
-              dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;
-              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5L5LV2J3');
-          `}
-        </Script>
-
         {/* ✅ Meta Pixel Code */}
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
@@ -68,7 +52,23 @@ export default function RootLayout({
       </head>
 
       <body className={inter.className + " bg-[#f9fff2] text-green-900"}>
-        {/* ✅ Google Tag Manager (noscript) */}
+        {/* ✅ Google Tag Manager (script in BODY) */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),
+              dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5L5LV2J3');
+          `}
+        </Script>
+
+        {/* ✅ GTM <noscript> fallback */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-5L5LV2J3"
