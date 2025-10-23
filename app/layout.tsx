e0749abd10ec5bd2d78292d9d5a7e9781e2d5a99
mohitagrawalmohit <mohitagarwal00112@gmail.com>
2025-10-23 14:40:03 +0530

    add html verification file

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
-
 import Footer from "@/components/Footer";
 import PageLoaderWrapper from "@/components/PageLoaderWrapper";
 import { Toaster } from "sonner";
