bc88a899c8f4159cb22f8808549448ae035f6244
mohitagrawalmohit <mohitagarwal00112@gmail.com>
2025-10-23 14:54:27 +0530

    add google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* ✅ Google Tag Manager (Head) */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),
+              dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5L5LV2J3');
+          `}
+        </Script>
+
         {/* ✅ Meta Pixel Code */}
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
@@ -40,7 +56,7 @@ export default function RootLayout({
           `}
         </Script>
 
-        {/* NoScript fallback (for browsers without JS) */}
+        {/* NoScript fallback for Meta Pixel */}
         <noscript>
           <img
             height="1"
@@ -52,11 +68,22 @@ export default function RootLayout({
       </head>
 
       <body className={inter.className + " bg-[#f9fff2] text-green-900"}>
+        {/* ✅ Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5L5LV2J3"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <PageLoaderWrapper>
           <Navbar />
           <main className="min-h-[calc(100vh-160px)]">{children}</main>
           <Footer />
         </PageLoaderWrapper>
+
         <Toaster richColors position="top-right" />
       </body>
     </html>
