6640a2fe884312b8aca0ca259e2c4820c0e651c5
mohitagrawalmohit <mohitagarwal00112@gmail.com>
2025-10-23 17:18:35 +0530

    add google analytics tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* ✅ Google Analytics (GA4) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-63F46B1LG5"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-63F46B1LG5');
+          `}
+        </Script>
+
         {/* ✅ Google Tag Manager (Head) */}
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`
