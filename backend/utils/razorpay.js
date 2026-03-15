339036c1ec8c0da79ade3f562876475734a4f4f6
mohitagrawalmohit <mohitagarwal00112@gmail.com>
2026-03-15 15:04:15 +0530

    add keyid and secret of razorpay

--- backend/utils/razorpay.js
+++ backend/utils/razorpay.js
@@ -1,8 +1,8 @@
 import Razorpay from "razorpay";
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_id: "rzp_live_SJ4U5mwzKkp2HD",
+  key_secret: "hBofLojl2J46r5Mt54NL097Y",
 });
 
 export default razorpay;
